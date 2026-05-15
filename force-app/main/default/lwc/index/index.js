c3589458c666c4c6210006c9f944655f91b7b2b7
manavsabarwal <manavsabarwal@gmail.com>
2026-05-15 19:12:04 +0530

    first comit

---
+++ force-app/main/default/lwc/index/index.js
@@ -0,0 +1,45 @@
+import { LightningElement} from 'lwc';
+import loginHelper from '@salesforce/apex/loginSignupController.loginHelper';
+import signupHelper from '@salesforce/apex/loginSignupController.signupHelper';
+
+export default class Index extends LightningElement {
+
+    username="";
+    password="";
+    signup=false;
+    
+    handleUsernameChange(event)
+    {
+        this.username=event.target.value;
+        console.log(this.username);
+    }
+
+    handlePasswordChange(event)
+    {
+        this.password=event.target.value;
+    }
+
+    showSignUp(event)
+    {
+        this.signup=true;
+    }
+
+    showlogin(event)
+    {
+        this.signup=false;
+    }
+
+    async login(event)
+    {
+        const result=await loginHelper({Username:this.username,Password:this.password});
+        console.log(result);
+
+    }
+
+    async signUp(event)
+    {
+        console.log('signup pressed');
+        const result=await signupHelper({Username:this.username,Password:this.password});
+        console.log(result);
+    }
+}
